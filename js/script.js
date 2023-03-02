ea964433996982012c2687c5c0d7cba6b014b80a
Misha.P <panikarmisha2003@gmail.com>
2023-03-02 13:53:06 +0200

    what i use at work, block complited

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__percent'),
+    lines = document.querySelectorAll('.skills__lines span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
